b63075b56825fc026c4860f96fa28e7c438dd1ac
Mike Leon <mleon@follett.com>
2017-06-07 14:13:40 -0500

    show selected movie in same page

--- src/app/movie-list-http/movie-list-http.component.ts
+++ src/app/movie-list-http/movie-list-http.component.ts
@@ -2,7 +2,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-
+import { Router } from '@angular/router';
 
 import { MovieHTTPService } from '../../app/movie-http.service';
 import { Movie } from '../../app/movie';
@@ -17,9 +17,10 @@ import { Movie } from '../../app/movie';
 })
 export class MovieListHTTPComponent implements OnInit {
   _movieHTTPList: Movie[];
+  _selectedMovie:Movie
   errorMessage: string;
      
-constructor(private movieHTTPListService: MovieHTTPService) {}
+  constructor(private movieHTTPListService: MovieHTTPService, private router: Router) {}
 //  constructor(private http: Http) { }
   
   ngOnInit() {
@@ -27,12 +28,22 @@ constructor(private movieHTTPListService: MovieHTTPService) {}
   //  this.http.get('http://localhost:8080/movies').map(response => response.json()).subscribe(result => this._movieHTTPList = result);
   }
   
-  
+  onSelect(movie: Movie): void {
+    this._selectedMovie = movie;
+  }
+
   get movieHTTPList() {
     return this._movieHTTPList;
   }
   set movieHTTPList(l) {
     this._movieHTTPList = l;
+  }
+
+  get selectedMovie() {
+    return this._selectedMovie;
+  }
+  set selectedMovie(m) {
+    this._selectedMovie = m;
   }
 
   logError(err) {
